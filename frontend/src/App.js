dadd8f8421961a8df48982cfcbfb8faceb4eafd8
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-02-26 21:55:49 +0500

    Updated 10:00 PM, 26 Feb, 2024

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Accordian from "./components/accordian/Accordian";
 import RandomColor from "./components/random_color/RandomColor.jsx";
+import StarRating from "./components/star_rating/StarRating.jsx";
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
 
       {/* Color Generator Component */}
       <RandomColor />
+
+      {/* Star Rating Component */}
+      <StarRating noOfStars={10} />
     </div>
   );
 }
