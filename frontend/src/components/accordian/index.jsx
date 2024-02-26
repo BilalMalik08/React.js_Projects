6ab09425bd2179c6f80982a6cf8238219b4cd686
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-02-26 16:41:02 +0500

    Updated 26 Feb 5 PM

--- frontend/src/components/accordian/index.jsx
+++ frontend/src/components/accordian/index.jsx
@@ -1,25 +1,42 @@
 // Single Selection
 // Multiple Selection
 
+import "./style.css";
 import data from "./data";
 import { useState } from "react";
 
 export default function Accordian() {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiSelect, setMultiSelect] = useState([]);
 
   function handleSingleSelection(getCurrentId) {
-    setSelected(getCurrentId);
+    setSelected(getCurrentId === selected ? null : getCurrentId);
   }
-  console.log(selected);
+
+  function handleMultiSelection(getCurrentId) {
+    let cpyMultiple = [...multiSelect];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+
+    console.log(findIndexOfCurrentId);
+  }
+
   return (
     <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item" key={dataItem.id}>
               <div
                 className="title"
-                onClick={() => handleSingleSelection(dataItem.id)}
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
               >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
