1d1da0d42a04414a0e69a28a4be8b9a09b57c4b2
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-02-28 03:19:41 +0500

    Updated 3:15 AM, 28 Feb, 2024

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,48 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
+import Accordian from "./components/accordian/Accordian";
+import ImageSlider from "./components/image_slider/ImageSlider.jsx";
+import LightDarkMode from "./components/light_dark_mode/LightDarkMode.jsx";
+import LoadMoreData from "./components/load_more_data/LoadMoreData.jsx";
+import QrCodeGenerator from "./components/qr_code_generator/QrCodeGenerator.jsx";
+import RandomColor from "./components/random_color/RandomColor.jsx";
+import ScrollIndicator from "./components/scroll_indicator/ScrollIndicator.jsx";
+import StarRating from "./components/star_rating/StarRating.jsx";
+import TreeView from "./components/tree_view/TreeView.jsx";
+import menu from "./components/tree_view/data.js";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* Accordian Component */}
+      <Accordian />
 
-      <Homes />
+      {/* Color Generator Component */}
+      <RandomColor />
+
+      {/* Star Rating Component */}
+      <StarRating noOfStars={10} />
+
+      {/* Image Slider Component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
+
+      {/* Load More Data Component */}
+      <LoadMoreData />
+
+      {/* Tree View Menu Component */}
+      <TreeView menu={menu} />
+
+      {/* QR Code Generator Component */}
+      <QrCodeGenerator />
+
+      {/* Light Dark Mode Component */}
+      <LightDarkMode />
+
+      {/* Custom Scroll Indicator Component */}
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
